4e5e2fcf8b98a3a5c5a544c356d0d1fc8f22a268
alexsokolov <frank9154@gmail.com>
2023-11-12 16:40:51 +0300

    First commit

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLightRed: UIView!
+    @IBOutlet var trafficLightYellow: UIView!
+    @IBOutlet var trafficLightGreen: UIView!
+    @IBOutlet var startNextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func StartNext(_ sender: Any) {
+    }
+    
 }
 
