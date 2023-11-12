dff7f7f8555df5ff00066a94a02167862ce75954
alexsokolov <frank9154@gmail.com>
2023-11-12 19:53:43 +0300

    pre-final comit

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,19 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var trafficLightRed: UIView!
     @IBOutlet var trafficLightYellow: UIView!
     @IBOutlet var trafficLightGreen: UIView!
     @IBOutlet var startNextButton: UIButton!
     
+    var currentLightIndex = 0
+    let lightColors: [UIColor] = [.red, .yellow, .green]
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        trafficLightRed.alpha = 0
+        trafficLightYellow.alpha = 0
+        trafficLightGreen.alpha = 0
     }
 
-    @IBAction func StartNext(_ sender: Any) {
+    @IBAction func StartNext(_ sender: UIButton) {
+        
+        if sender.currentTitle == "START" {
+            trafficLightRed.alpha = 0.3
+            trafficLightYellow.alpha = 0.3
+            trafficLightGreen.alpha = 0.3
+            startNextButton.setTitle("NEXT", for: .normal)
+            
+        } else {
+            currentLightIndex += 1
+            if currentLightIndex >= lightColors.count {
+                currentLightIndex = 0
+            }
+            
+            switch currentLightIndex {
+            case 0:
+                trafficLightRed.alpha = 1
+                trafficLightYellow.alpha = 0.3
+                trafficLightGreen.alpha = 0.3
+            
+            case 1:
+                trafficLightRed.alpha = 0.3
+                trafficLightYellow.alpha = 1
+                trafficLightGreen.alpha = 0.3
+                
+            case 2:
+                trafficLightRed.alpha = 0.3
+                trafficLightYellow.alpha = 0.3
+                trafficLightGreen.alpha = 1
+               
+            default:
+                break
+                
+            }
+            
+        }
     }
     
 }
